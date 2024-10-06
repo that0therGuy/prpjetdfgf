4a035439e83003b7c24512964832a31031160b26
that0therGuy <111861337+that0therGuy@users.noreply.github.com>
2024-10-06 14:56:02 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,26 @@
+setInterval(answer,100)
+
+function answer(){
+    let input=document.querySelector('.field').value;
+    calculator.setExpression({ id: 'line1', latex: input, color: '#ff0000' });
+    input=input.replace(/pi/gi,Math.PI)
+    input=input.replace(/phi/gi,(Math.sqrt(5)+1)/2)
+   
+    input = input.replace(/sqrt\(([^)]+)\)/gi, 'Math.sqrt($1)');
+    input = input.replace(/sin\(([^)]+)\)/gi, 'Math.sin($1)'); 
+    input = input.replace(/tan\(([^)]+)\)/gi, 'Math.tan($1)'); 
+    input = input.replace(/cos\(([^)]+)\)/gi, 'Math.cos($1)'); 
+    input = input.replace(/log\(([^)]+)\)/gi, 'Math.log($1)');
+
+    document.querySelector('p').innerText='='+eval(input);
+    if (document.querySelector('p').innerText=='=undefined'){
+        document.querySelector('p').style.opacity='0';
+        document.querySelector('p').style.display='none';     
+        
+    }else{
+        document.querySelector('p').style.opacity='1';
+        document.querySelector('p').style.display='inline-block';     
+    }
+    
+
+}
